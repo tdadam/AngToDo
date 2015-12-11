f4b110aa8350b3bfccbad7704f89785a15fb7566
Tim Adam <tdadam@xactware.com>
2015-12-11 10:30:21 -0700

    initial adding task functionality started

--- js/basicController.js
+++ js/basicController.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict';
 
     angular.module('basicController', [])
@@ -10,11 +10,22 @@
 
         // list everything
         var bc = this;
-        bc.sometext = 'default value';
         bc.doSomething = doSomething;
+
+        // options in the dropdown for item type
+        bc.data = {
+            availableOptions: [
+                {id: '1', name: 'Work'},
+                {id: '2', name: 'School'},
+                {id: '3', name: 'Family'},
+                {id: '4', name: 'Shopping'}
+            ]
+        };
 
         // define functions
         function doSomething() {
+            alert(bc.sometext + " " + bc.listType);
+            bc.listType = '';
             bc.sometext = '';
         }
     }
