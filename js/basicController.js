3161d06dc7b168eecfa152bfa05add2780416d12
Tim Adam <tdadam@xactware.com>
2015-12-16 10:30:39 -0700

    fixed a lot of date issues, still need lists

--- js/basicController.js
+++ js/basicController.js
@@ -8,24 +8,29 @@
 
     function basicController() {
 
+
+
         // list everything
         var bc = this;
         bc.doSomething = doSomething;
         bc.clearDone = clearDone;
         bc.deleteTask = deleteTask;
-        //bc.doList = [{'title':'placeholder', 'type':1, 'done':false}];
+        bc.addList = addList;
+        bc.tooLate = tooLate;
         bc.minDate = new Date();
-        console.log(bc.minDate);
+        bc.testDate = new Date('2015-10-17');
+        var earliest = bc.minDate.getTime();
 
         bc.listNames = [];
-        bc.doList = [];
+        bc.doList = [{'title':'testing', 'due':bc.testDate, 'type':bc.listType, 'done':false, 'past':false}];
 
         // define functions
         function doSomething() {
-            bc.doList.push({'title':bc.sometext, 'due':bc.dateDue, 'type':bc.listType, 'done':false});
+            bc.doList.push({'title':bc.sometext, 'due':bc.dateDue, 'type':bc.listType, 'done':false, 'past':false});
             bc.listType = '';
             bc.dateDue = '';
             bc.sometext = '';
+            bc.tooLate();
         }
         function clearDone(){
             bc.doList = bc.doList.filter(function(item){
@@ -36,5 +41,15 @@
             var index = bc.doList.indexOf(task);
             bc.doList.splice(index, 1);
         }
+        function addList(){
+
+        }
+        function tooLate(){
+            for (var i = 0; i < bc.doList.length; i++){
+                var taskDue = bc.doList[i].due.getTime();
+                (earliest > taskDue) ? bc.doList[i].past = true : bc.doList[i].past = false;
+            }
+        }
+        bc.tooLate();
     }
 }());
